2de31388f44127bf2454bfbe3bb58c120c386093
sagarus2020 <juanpablocq@ufps.edu.co>
2025-07-29 17:27:38 -0500

    first commit

---
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.domain.Persona;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ControladorInicio {
+    @Value("${index.saludo}")
+    private String saludo;
+    @GetMapping("/")
+    public String inicio(Model model){
+        var persona =new Persona();
+        persona.setNombre("juan");
+        persona.setApellido("cardenas");
+        persona.setEmail("juancardenas@test.com");
+        persona.setTelefono("7777344645");
+
+
+        var mensaje="hola mundo thymeleaf";
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
+
+        log.info("ejecutando");
+        log.debug("mas detalle");
+        return "index";
+    }
+    
+}
