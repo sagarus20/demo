cae5b35237105b24188e27b1e2694276910857f5
sagarus20 <juanpablocardenasq@gmail.com>
2025-08-30 16:15:40 -0500

    last spring lesson of bootstrap

--- src/main/java/com/gm/web/ControladorInicio.java
+++ src/main/java/com/gm/web/ControladorInicio.java
@@ -33,7 +33,12 @@ public class ControladorInicio {
         log.info("ejecutando lista de controlador spring MVC");
         log.info("usuario que hizo login:" +user);
         model.addAttribute("personas", personas);
-        
+        var saldoTotal=0D;
+        for(var p:personas){
+            saldoTotal+=p.getSaldo();
+        }
+        model.addAttribute("saldoTotal", saldoTotal);
+        model.addAttribute("totalClientes", personas.size());
         return "index";
     }
     @GetMapping("/agregar")
